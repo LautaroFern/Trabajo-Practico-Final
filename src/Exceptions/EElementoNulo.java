37457698b20b62c20688ad7ef493c25fe37ef7a8
Lautaro Fernandez Esposito <lfernandezesposito@gmail.com>
2025-10-29 19:21:40 -0300

    Agregada la libreria json y modelada la clase habitacion y creada una exception

---
+++ src/Exceptions/EElementoNulo.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class EElementoNulo extends Exception {
+    public EElementoNulo(String message) {
+        super(message);
+    }
+}
--- src/Models/Habitacion.java
+++ src/Models/Habitacion.java
@@ -1,4 +1,40 @@
 package Models;
 
+import Exceptions.EElementoNulo;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Habitacion {
+    private String nombre;
+    private ArrayList<PistaTexto> pistas;
+    private ArrayList<ObjetoCasa> objetos;
+    private Integer idHabitacion;
+
+    public Habitacion(String nombre, Integer idHabitacion) {
+        this.nombre = nombre;
+        this.idHabitacion = idHabitacion;
+        this.pistas = new ArrayList<>();
+        this.objetos = new ArrayList<>();
+    }
+
+    public boolean agregarPista(PistaTexto pista) throws EElementoNulo {
+        if (pista == null){
+            throw new EElementoNulo("La pista ingresada no puede ser nula");
+        } else {
+            pistas.add(pista);
+            return true;
+        }
+    }
+
+    public boolean agregarObjeto(ObjetoCasa objetoCasa) throws EElementoNulo{
+        if (objetoCasa == null){
+            throw new EElementoNulo("El objeto ingresado no puede ser nulo");
+        } else {
+            objetos.add(objetoCasa);
+            return true;
+        }
+    }
+
+
 }
