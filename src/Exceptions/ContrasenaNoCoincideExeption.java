1e6fc3ca179eee9d56dcd515666f3c5d140b8f0b
sltobias7 <sltobias7@gmail.com>
2025-10-29 19:42:11 -0300

    Clase jugador

---
+++ src/Exceptions/ContrasenaNoCoincideExeption.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class ContrasenaNoCoincideExeption extends RuntimeException {
+    public ContrasenaNoCoincideExeption(String message) {
+        super(message);
+    }
+}
--- src/Models/Jugador.java
+++ src/Models/Jugador.java
@@ -1,4 +1,77 @@
 package Models;
 
+import Collections.Inventario;
+import Exceptions.ContrasenaNoCoincideExeption;
+
+import java.util.Objects;
+
 public class Jugador {
+    private String nombre;
+    private String usuario;
+    private Integer contrasena;
+    private Inventario inventario;
+    private double progreso;
+
+    public Jugador(String nombre, String usuario, Integer contrasena, Inventario inventario, double progreso) {
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.inventario = inventario;
+        this.progreso = progreso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public Integer getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(Integer contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public Inventario getInventario() {
+        return inventario;
+    }
+
+    public double getProgreso() {
+        return progreso;
+    }
+
+
+    public boolean cambiarContrasena(Integer contrasenaActual, Integer nuevaContrasena) throws ContrasenaNoCoincideExeption {
+        if (this.contrasena.equals(contrasenaActual)) {
+            this.contrasena = nuevaContrasena;
+            return true;
+        }
+        throw new ContrasenaNoCoincideExeption("La contraseña no coincide");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Jugador jugador)) return false;
+        return Objects.equals(contrasena, jugador.contrasena);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(contrasena);
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador: \n" +
+                "Nombre= " + nombre + '\'' +
+                "Usuario='" + usuario + '\'' +
+                "Contrasena=" + contrasena + '\'' +
+                "Inventario=" + inventario + '\'' +
+                "Progreso=" + progreso +
+                '\n';
+    }
 }
